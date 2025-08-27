2e2024fbb1f44003af35dcf6dcb44383f960267c
Sakolkiat <sakolkiat.n@gmail.com>
2025-08-27 13:02:53 +0700

    Add taskInput window

--- src/content/data/createTask.js
+++ src/content/data/createTask.js
@@ -1,20 +1,35 @@
-
 export default class AddTaskFormHandler {
     // create form elements
     inputContainer() {
         // Input container
-        const container = document.createElement('div');
+        const container = document.createElement('form');
         container.classList.add('input-container');
 
         // Left container
         const leftInputContainer = document.createElement('div');
         leftInputContainer.classList.add('text-input-container');
 
+        // text input
+        const titleInput = document.createElement('input');
+        titleInput.classList.add('title-input');
+        titleInput.placeholder = 'Task title';
+        titleInput.setAttribute('type', 'text');
+        titleInput.setAttribute('required', '');
+
+        // desc input
+        const descInput = document.createElement('input');
+        descInput.classList.add('desc-input');
+        descInput.placeholder = 'Task Details';
+        descInput.setAttribute('type', 'text');
+
+        leftInputContainer.append(titleInput, descInput);
+
         const leftBtnContainer = document.createElement('div');
         leftBtnContainer.classList.add('btns-input-container', 'left');
 
         const leftContainer = document.createElement('div');
         leftContainer.classList.add('task-input-container');
+
 
         leftContainer.append(leftInputContainer, leftBtnContainer);
 
@@ -26,48 +41,14 @@ export default class AddTaskFormHandler {
         rightBtnContainer.classList.add('btn-input-container', 'right');
 
         rightContainer.append(rightBtnContainer);
-
         // Append btn
-        return container.append(leftContainer, rightContainer);
-    }
-
-    createBtn(name, className, id) {
-        const btn = document.createElement('button');
-        btn.classList.add(className);
-        btn.setAttribute('id', id);
-        btn.textContent = name;
-
-        return btn;
-    }
-
-    inputContainer() {
-        // title
-        const titleInput = document.createElement('input');
-        titleInput.classList.add('title-input');
-        titleInput.placeholder = 'title test';
+        container.append(leftContainer, rightContainer);
         
-        
-        // description
-        const descInput = document.createElement('input');
-        descInput.classList.add('desc-input');
-        descInput.placeholder = 'description input';
-        
-        const inputContainer = this.selectElement('text-input-container');
-        inputContainer.append(titleInput, descInput);
+        return container;
     }
 
-    // get element 
-    selectElement(className) {
-        const target = document.querySelector(className);
-        return target;
+    renderAddTask(target) {
+        const addTaskContainer = this.inputContainer();
+        target.append(addTaskContainer);
     }
-
-
-    // if receive input
-    // create a new task (obj)
-    // send to taskData
-
-    // if cancel
-    // remove this container
-
 }
--- src/content/display.js
+++ src/content/display.js
@@ -1,12 +1,17 @@
 import renderSidebar from "./sidebar";
 import { renderContent } from "./content";
 import { getButtonId } from "./app/updateDisplay";
+import AddTaskFormHandler from "./data/createTask";
 
 export default function renderScreen() {
     const target = document.querySelector('.sidebar');
     const contentTarget = document.querySelector('.content');
+    const addTaskWin = new AddTaskFormHandler();
     
     renderSidebar(target);
     renderContent(contentTarget);
     getButtonId();
+
+    const taskContainerTarget = document.querySelector('.tasks-container');
+    addTaskWin.renderAddTask(taskContainerTarget);
 }
