5c7f5c04e966a328afaaeae2501ad10aaa0bf0ad
Sakolkiat <sakolkiat.n@gmail.com>
2025-08-30 23:58:59 +0700

    Add show current task value when click edit button

--- src/content/app/taskCardHandler.js
+++ src/content/app/taskCardHandler.js
@@ -29,15 +29,38 @@ export default class TaskCardHandler {
         }
     }
 
-    editTask() {
+    editTask(taskId, cardTargetElement) {
+        const taskIndex = this.tasks.data.findIndex(obj => obj.id === taskId);
+
+        // current task value
+        const name = this.tasks.data[taskIndex].name;
+        const desc = this.tasks.data[taskIndex].desc;
+        const date = this.tasks.data[taskIndex].date;
+        const prio = this.tasks.data[taskIndex].priority;
+        const tag  = this.tasks.data[taskIndex].tag;
+
+        // get card data value
+        const titleEle = cardTargetElement.querySelector('.title-edit');
+        const descEle  = cardTargetElement.querySelector('.desc-edit');
+        const dateEle  = cardTargetElement.querySelector('.date-edit');
+        const prioEle  = cardTargetElement.querySelector('.prio-edit');
+        const tagEle   = cardTargetElement.querySelector('.tag-edit');
+
+        titleEle.value = name;
+        descEle.value = desc;
+        dateEle.value = date;
+        prioEle.value = prio;
+        tagEle.value = tag;
 
     }
 
+    // click event action in tasks container
     TaskBtn(event) {
         const taskCard = event.target.closest('.task-container');
         if (!taskCard) return;
         // element id
         const taskId = taskCard.dataset.id;
+
 
         // checkbox button
         if (event.target.closest('.task-chbx')){
@@ -55,9 +78,13 @@ export default class TaskCardHandler {
 
         // edit btn
         if (event.target.closest('.edit-task')) {
-            console.log('lmao');
+            // remove card content and show edit task input
             removeCardElements(taskCard);
             taskCard.append(editInputContainer());
+
+            // this.editTask(taskId); // receive current value
+            // getCurrentTaskData(taskCard); // edit input text
+            this.editTask(taskId, taskCard);
         }
     }
 }
@@ -66,6 +93,19 @@ function removeCardElements(taskContainer) {
     taskContainer.innerHTML = '';
 }
 
-function showTaskInput() {
+function getCurrentTaskData(cardTargetElement) {
+    // get card data value
+    const titleEle = cardTargetElement.querySelector('.title-edit');
+    const descEle  = cardTargetElement.querySelector('.desc-edit');
+    const dateEle  = cardTargetElement.querySelector('.date-edit');
+    const prioEle  = cardTargetElement.querySelector('.prio-edit');
+    const tagEle   = cardTargetElement.querySelector('.tag-edit');
+}
+
+function getDataFromValue() {
+
+}
+
+function newCardValueDisplay() {
 
 }
