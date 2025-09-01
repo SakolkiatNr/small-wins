47d9ff2c606e0248b618614be5f7ec17f31aa39e
Sakolkiat <sakolkiat.n@gmail.com>
2025-09-01 21:32:24 +0700

    Create projectBtn elements

---
+++ src/content/app/projectBtn.js
@@ -0,0 +1,26 @@
+function createProjectBtn(projectObj) {
+    const btn = document.createElement('button');
+    btn.classsList.add('project-btn');
+    btn.textContent = projectObj.name;
+
+    return btn;
+}
+
+function deleteProjectBtn() {
+    const btn = document.createElement('button');
+    btn.classList.add('delete-project');    
+
+    return btn;
+}
+
+export function projectBtnContainer(projectObj) {
+    const container = document.createElement('div');
+    container.classList.add('project-cnt');
+    container.dataset.id = projectObj.id;
+
+    container.append(createProjectBtn(projectObj),
+                     deleteProjectBtn()
+                    );
+
+    return container;
+}
